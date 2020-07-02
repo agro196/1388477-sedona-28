1bd735d447266253a8783251741598b3fe1493d4
agro196 <agro_06@mail.ru>
2020-07-02 21:58:02 +0300

    Отправка на защиту2

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
-var form = document.querySelector('.hotel-search-form');
-var form_show_button = document.querySelector('.hotel-search-button');
-var arrival_date = form.querySelector('input[name="arrival-date"]');
-var departure_date = form.querySelector('input[name="departure-date"]');
-var adults = form.querySelector('input[name="adults"]');
+var form = document.querySelector(".hotel-search-form");
+var form_show_button = document.querySelector(".hotel-search-button");
+var arrival_date = form.querySelector("input[name=\"arrival-date\"]");
+var departure_date = form.querySelector("input[name=\"departure-date\"]");
+var adults = form.querySelector("input[name=\"adults\"]");
 form_show_button.addEventListener("click", function () {
-    form.classList.toggle('form-shown');
-    form.classList.toggle('form-hidden');
+    form.classList.toggle("form-shown");
+    form.classList.toggle("form-hidden");
     form.classList.remove("form-error");
 });
 form.addEventListener("submit", function (evt) {
